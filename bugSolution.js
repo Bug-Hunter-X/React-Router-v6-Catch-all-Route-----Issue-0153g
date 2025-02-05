f4fc989f44b2f2992ad3c3bdb119eba88c9f77c3
BugHunter-X <askzenai@gmail.com>
2025-02-05 07:10:05 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        {/* The catch-all route should be placed last */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+function Home() {/* ... */}
+function About() {/* ... */}
+function NotFound() {/* ... */}
